fa70dcb78ab09311063a34380641f2942321f948
Aleh Kot <kot.oleg@gmail.com>
2020-12-08 08:02:54 -0500

    Fix response handling

--- handler.js
+++ handler.js
@@ -5,23 +5,31 @@ const got = require('got');
 module.exports.time = async (event) => {
   try {
     const { id } = event.pathParameters;
-    const response = await got(
-      `http://rushogwarts.myarena.ru/time/${parseInt(id)}`
-    );
+
+    // Check if number.
+    if (!/^\d+$/.test(id))
+      return {
+        statusCode: 400,
+        body: 'Invalid time provided',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      };
+
+    const targetUrl = `http://rushogwarts.myarena.ru/time/${id}`;
+    const response = await got(targetUrl);
     return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify({
-        ...response.body,
-      }),
+      body: response.body,
     };
   } catch (err) {
     console.error(err);
     return {
       statusCode: 502,
-      body: JSON.stringify({ message: 'Unable to reach upstream server' }),
+      body: 'Unable to reach upstream server',
       headers: {
         'Access-Control-Allow-Origin': '*',
       },
